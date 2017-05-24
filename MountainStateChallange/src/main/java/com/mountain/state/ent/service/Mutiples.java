844aed4211768ed539583a9c6dab9f509c4d0c7a
kumar <muppana.kumar59@gmail.com>
2017-05-24 01:15:28 -0500

    Multiple class updated

--- MountainStateChallange/src/main/java/com/mountain/state/ent/service/Mutiples.java
+++ MountainStateChallange/src/main/java/com/mountain/state/ent/service/Mutiples.java
@@ -21,19 +21,19 @@ public class Mutiples {
 
 		for (int i = 1; i < 67; i++) {
 			int multple3 = 3 * i;
-			if (multple3 > input1 && multple3 < input2) {
+			if (multple3 > input1 && multple3 <= input2) {
 				resultMap.put(multple3, "ME");
 			}
 		}
 		for (int i = 1; i < 29; i++) {
 			int multple7 = 7 * i;
-			if (multple7 > input1 && multple7 < input2) {
+			if (multple7 > input1 && multple7 <= input2) {
 				resultMap.put(multple7, "MS3");
 			}
 		}
 		for (int i = 1; i < 10; i++) {
 			int multple3and7 = 21 * i;
-			if (multple3and7 > input1 && multple3and7 < input2) {
+			if (multple3and7 > input1 && multple3and7 <= input2) {
 				resultMap.put(multple3and7, "MS3 and ME");
 			}
 		}
